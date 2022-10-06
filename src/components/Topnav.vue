28bb35c950970354ce3c81791d14061477bc75c8
Latrevion <xiaopikaqiu@outlook.com>
2022-10-06 16:00:24 +0800

    添加 toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,14 +1,12 @@
 <template>
-    <div>
-       <div class="topnav">
-        <div class="logo" @click="toggleMenu">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-       </div>
-       <span class="toggleAside"></span>
-    </div>
+  <div class="topnav">
+    <div class="logo" @click="toggleMenu">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+    <span class="toggleAside"></span>
+  </div>
 </template>
 <script lang="ts">
   import { inject, Ref } from 'vue'
@@ -26,31 +24,39 @@
 </script>
 
 <style lang="scss" scoped>
-    .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  position: relative;
-  z-index: 10;
-  justify-content: center;
-  align-items: center;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
+  .topnav {
+    background: pink;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: relative;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li {
+        margin: 0 1em;
+      }
+    }
+    > .toggleAside {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      background: skyblue;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    @media (max-width:500px) {
+      > .menu{display: none;} 
+      > .logo{margin: 0 auto;}
     }
   }
-  > .toggleMenu {}
-  @media (max-width:500px) {
-    > .menu{display: none;} 
-    > .logo{margin: 0 auto;}
-  }
-
-}
-</style>
+  </style>
