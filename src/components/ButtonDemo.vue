d1a469ff10a842f2ac1ab8baf6c99322573e6b93
Latrevion <xiaopikaqiu@outlook.com>
2022-10-08 21:15:36 +0800

    创建button组件雏形

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        Button实例
-    </div>
+    <div>Button实例</div>
     <h1>实例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onClick"
+         @focus="onClick" 
+         @mouseover="onClick" size="small">你好</Button>
     </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue';
 export default{
-components:{Button:Button}
+components:{Button:Button},
+setup(){
+    const onClick =()=>{
+        console.log('h1')
+    }
+    return {onClick}
+}
 } 
 </script>
