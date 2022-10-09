1c84b62b6de8538069afcbd46b70fb5c7d3dbaca
Latrevion <xiaopikaqiu@outlook.com>
2022-10-09 11:08:51 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -45,5 +45,22 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.forest-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.forest-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
