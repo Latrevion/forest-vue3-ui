995b7ba62db3f5f80f310531a87069c60a42bebb
Latrevion <xiaopikaqiu@outlook.com>
2022-10-10 17:03:08 +0800

    让Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,30 @@
 <template>
     <div>Dialog 示例</div>
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnclickOverlay="true" :ok="fn1" :cancel="fn2"></Dialog>
 </template>
 
 <script lang="ts">
-    import Dialog from '../lib/Dialog.vue';
+    import Button from '../lib/Button.vue'
+    import Dialog from '../lib/Dialog.vue'
+    import {ref } from 'vue'
     export default{
-        components:{Dialog},
-        
+        components:{Dialog,Button},
+        setup(){
+            const x = ref(false)
+            const toggle = ()=>{
+                x.value = !x.value
+            }
+
+            const fn1 =()=>{
+                return  false
+            }
+
+            const fn2 =()=>{
+                return false
+            }
+            return {x,toggle,fn1,fn2}
+        }
     }
 </script>
