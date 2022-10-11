a8fb0fafbadbf394f3df9783ed09015091ceeb9f
Latrevion <xiaopikaqiu@outlook.com>
2022-10-11 21:18:27 +0800

    indicator css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="forest-tabs">
         <div class="forest-tabs-nav">
-            <div class="forest-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}
+            <div class="forest-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)"
+                :class="{selected:t===selected}" :key="index">
+                {{t}}
             </div>
+            <div class="forest-tabs-nav-indicator"></div>
         </div>
         <div class="forest-tabs-content">
-        <component class="forest-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"></component>
-    </div>
+            <component class="forest-tabs-content-item" :class="{selected: c.props!.title === selected }"
+                v-for="c in defaults" :is="c" />
+        </div>
     </div>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
+import { computed } from '@vue/runtime-core';
 export default {
+    props: {
+        selected: {
+            type: String
+        }
+    },
     setup(props, context) {
         const defaults = context.slots.default!()
         defaults.forEach((tag) => {
@@ -20,11 +30,24 @@ export default {
             }
         })
 
+        const current = computed(() => {
+            //filter返回个数组，哪怕数据只有一个
+            return defaults.filter((tag) => {
+                return tag.props!.title === props.selected
+            })[0]
+        })
+
         const titles = defaults.map((tag) => {
             return tag.props!.title
         })
+
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+        }
+
+
         return {
-            defaults, titles
+            defaults, titles, current, select
         }
 
     }
@@ -35,25 +58,49 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .forest-tabs {
-  &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-      &:first-child {
-        margin-left: 0;
-      }
-      &.selected {
-        color: $blue;
-      }
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+        position: relative;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            width: 100px;
+        }
+
     }
-  }
-  &-content {
-    padding: 8px 0;
-  }
+
+    &-content {
+        padding: 8px 0;
+
+        &-item {
+            display: none;
+
+            &.selected {
+                display: block;
+            }
+        }
+    }
 }
 </style>
