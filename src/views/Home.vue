1fceb9128007e5701f8c1b2b3c576c8d16383572
Latrevion <xiaopikaqiu@outlook.com>
2022-10-12 17:37:10 +0800

    使用本地icon-js，引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,38 +9,65 @@
                 <router-link to="/doc">开始</router-link>
             </p>
         </div>
+        <div class="features">
+            <svg class="icon">
+                <use xlink:href="#icon-vue"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-typescript"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-shandian"></use>
+            </svg>
+        </div>
     </div>
 </template>
 <script lang="ts">
-    import Topnav from '../components/Topnav.vue'
-    export default{
-        components:{Topnav}
-    }
+import Topnav from '../components/Topnav.vue'
+export default {
+    components: { Topnav }
+}
 </script>
 
 <style lang="scss" scoped>
-.topnavAndBanner{
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
+.topnavAndBanner {
     background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
 }
 
-.banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+.features {
+    >svg {
+        width: 64px;
+        height: 64px;
+    }
+}
 
-  > .actions{
-    padding: 8px 0;
-    a {
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
-    }}}
-  
+.banner {
+    color: $color;
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: $green;
+            color: white;
+            display: inline-block;
+            padding: 8px 24px;
+            border-radius: $border-radius;
+
+            &:hover {
+                text-decoration: none;
+            }
+        }
+    }
+}
 </style>
