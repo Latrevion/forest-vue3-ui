cd1a3ea18596fba516f0104dd6d5776599b6ee7f
Latrevion <xiaopikaqiu@outlook.com>
2022-10-12 15:21:42 +0800

    再次优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,128 +1,114 @@
 <template>
-    <div class="forest-tabs">
-        <div class="forest-tabs-nav" ref="container">
-            <div class="forest-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }"
-                @click="select(t)" :class="{selected:t===selected}" :key="index">
-                {{t}}
-            </div>
-            <div class="forest-tabs-nav-indicator" ref="indicator"></div>
-        </div>
-        <div class="forest-tabs-content">
-            <component class="forest-tabs-content-item" :class="{selected: c.props.title === selected }"
-                v-for="c in defaults" :is="c" />
-        </div>
+  <div class="forest-tabs">
+    <div class="forest-tabs-nav" ref="container">
+      <div class="forest-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="forest-tabs-nav-indicator" ref="indicator"></div>
     </div>
+    <div class="forest-tabs-content">
+      <component class="forest-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+
+    </div>
+  </div>
 </template>
+
 <script lang="ts">
-import Tab from './Tab.vue'
-import { computed, ref, onMounted ,onUpdated} from 'vue';
+import Tab from './Tab.vue';
+import {
+  computed,
+  ref,
+  watchEffect
+} from 'vue';
+
 export default {
-    props: {
-        selected: {
-            type: String
-        }
-    },
-    setup(props, context) {
-  
-        const selectedItem =ref<HTMLDivElement>(null)
-        const indicator = ref<HTMLDivElement>(null)
-        const container = ref<HTMLDivElement>(null)
-        const x = ( )=>{ 
-       
-    
-            const { width } = selectedItem.value.getBoundingClientRect()
-            indicator.value.style.width = width + 'px'
-            const { left: left1 } = container.value.getBoundingClientRect()
-            const { left: left2 } = selectedItem.value.getBoundingClientRect()
-            const left = left2 - left1
-            indicator.value.style.left =left +'px'
-        }
-
-        onMounted(x)
-        onUpdated(x)
-
-
-
-
-        const defaults = context.slots.default!()
-        defaults.forEach((tag) => {
-            if (tag.type !== Tab) {
-                throw new Error('Tabs 子标签必须是Tab')
-            }
-        })
-
-        const current = computed(() => {
-            //filter返回个数组，哪怕数据只有一个
-            return defaults.filter((tag) => {
-                return tag.props!.title === props.selected
-            })[0]
-        })
-
-        const titles = defaults.map((tag) => {
-            return tag.props!.title
-        })
-
-        const select = (title: string) => {
-            context.emit('update:selected', title)
-        }
-
-
-        return {
-            defaults, titles, current, select, selectedItem, indicator, container
-        }
-
+  props: {
+    selected: {
+      type: String
     }
-}
+  },
+  setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+
+    watchEffect(() => {
+      if(!selectedItem.value||!container.value) return
+      const {
+        width
+      } = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {
+        left: left1
+      } = container.value.getBoundingClientRect();
+      const {
+        left: left2
+      } = selectedItem.value.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    });
+
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab');
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {
+      defaults,
+      titles,
+      select,
+      selectedItem,
+      indicator,
+      container
+    };
+  }
+};
 </script>
 
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
-
 .forest-tabs {
-    &-nav {
-        display: flex;
-        color: $color;
-        border-bottom: 1px solid $border-color;
-        position: relative;
-
-        &-item {
-            padding: 8px 0;
-            margin: 0 16px;
-            cursor: pointer;
-
-            &:first-child {
-                margin-left: 0;
-            }
-
-            &.selected {
-                color: $blue;
-            }
-        }
-
-        &-indicator {
-            position: absolute;
-            height: 3px;
-            background: $blue;
-            left: 0;
-            bottom: -1px;
-            width: 100px;
-            transition: all 250ms;
-        }
-
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
     }
-
-    &-content {
-        padding: 8px 0;
-
-        &-item {
-            display: none;
-
-            &.selected {
-                display: block;
-            }
-        }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
 }
 </style>
