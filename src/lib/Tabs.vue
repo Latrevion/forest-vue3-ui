1ce478c999177350a2fb7d99f11cc050e413b680
Latrevion <xiaopikaqiu@outlook.com>
2022-10-12 15:43:12 +0800

    优化tab切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="forest-tabs">
     <div class="forest-tabs-nav" ref="container">
-      <div class="forest-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      <div class="forest-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }"
+        @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
       <div class="forest-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="forest-tabs-content">
-      <component class="forest-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
-
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
@@ -16,6 +16,7 @@ import Tab from './Tab.vue';
 import {
   computed,
   ref,
+  onMounted,
   watchEffect
 } from 'vue';
 
@@ -30,21 +31,23 @@ export default {
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
 
-    watchEffect(() => {
-      if(!selectedItem.value||!container.value) return
-      const {
-        width
-      } = selectedItem.value.getBoundingClientRect();
-      indicator.value.style.width = width + 'px';
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect();
-      const {
-        left: left2
-      } = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + 'px';
-    });
+    onMounted(() => {
+      watchEffect(() => {
+
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + 'px';
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect();
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + 'px';
+      })
+    })
 
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -52,6 +55,11 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
+
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
+
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
@@ -64,7 +72,8 @@ export default {
       select,
       selectedItem,
       indicator,
-      container
+      container,
+      current
     };
   }
 };
@@ -74,23 +83,28 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .forest-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
     position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
     &-indicator {
       position: absolute;
       height: 3px;
@@ -101,14 +115,9 @@ $border-color: #d9d9d9;
       transition: all 250ms;
     }
   }
+
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
