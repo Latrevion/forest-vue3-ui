b6cb016f1e425714071fb6fcf26628c306543d2c
Latrevion <xiaopikaqiu@outlook.com>
2022-10-15 16:55:25 +0800

    填充文档内容

--- src/views/IntroDemo.vue
+++ src/views/IntroDemo.vue
@@ -1,3 +1,9 @@
 <template>
-    Intro 介绍
+    <article>
+        <h1>介绍</h1>
+        <p>Forest UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+        <p>这款组件库本人为了总结Vue3相关学习感受而写的，全程亲手编写，尽量不采用第三方库实现</p>
+        <p>目前仍在测试迭代升级，处于Beta阶段，仅供学习，暂不推荐本 UI 库用于实际生产环境。目前仅支持 Vue3，推荐您使用 setup 语法糖，如果你在使用 Vue2，建议使用别的 UI 库。</p>
+        下一节：<a href="#/doc/install">安装</a>
+    </article>
 </template>
