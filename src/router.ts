74e66ec8f4ce42ede97a32d112e12cf1997972c4
Latrevion <xiaopikaqiu@outlook.com>
2022-10-16 11:45:10 +0800

    再三优化

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,11 @@ import DocDemo from './components/DocDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-import IntroDemo from './views/IntroDemo.vue'
-import InstallDemo from './views/InstallDemo.vue'
-import BeginDemo from './views/BeginDemo.vue'
+import Markdown from "./components/Markdown.vue";
+import { h } from 'vue'
+const md =(filename)=>{
+  return  h(Markdown,{path:`../markdown/${filename}.md`,key:filename})
+}
 
 const history =createWebHashHistory()
 export const router  =createRouter({
@@ -18,9 +20,16 @@ export const router  =createRouter({
         {path:'/doc',component:Doc,
         children:[
             {path:'',component:DocDemo},
-            {path:'intro',component:IntroDemo},
-            {path:'install',component:InstallDemo},
-            {path:'begin',component:BeginDemo},
+            
+        
+            {path:'intro',component:md('intro')},
+
+     
+            {path:'install',component:md('install')},
+
+       
+            {path:'begin',component:md('begin')},
+
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
