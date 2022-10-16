d1c8763b83c3de6f9027b5b1ace453fcc621d706
Latrevion <xiaopikaqiu@outlook.com>
2022-10-16 17:46:52 +0800

    使用component优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,82 +1,84 @@
 <template>
     <div>
-      <h1>Switch 组件示例 </h1>
-      <div class="demo">
-        <h2>常规用法</h2>
-        <div class="demo-component">
-          <Switch1Demo></Switch1Demo>
+        <h1>Switch 组件示例 </h1>
+        <div class="demo">
+            <h2>常规用法</h2>
+            <div class="demo-component">
+               <component :is="Switch1Demo"></component>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre>{{Switch1Demo.__sourceCode}}</pre>
+            </div>
         </div>
-        <div class="demo-actions">
-          <Button>查看代码</Button>
+        <div class="demo">
+            <h2>支持 disabled </h2>
+            <div class="demo-component">
+                <component :is="Switch2Demo"></component>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre>{{Switch2Demo.__sourceCode}}</pre>
+            </div>
         </div>
-        <div class="demo-code">
-          <pre>{{Switch1Demo.__sourceCode}}</pre>
-        </div>
-      </div>
-      <div class="demo">
-        <h2>支持 disabled </h2>
-        <div class="demo-component">
-          <Switch2Demo></Switch2Demo>
-        </div>
-        <div class="demo-actions">
-          <Button>查看代码</Button>
-        </div>
-        <div class="demo-code">
-          <pre>{{Switch2Demo.__sourceCode}}</pre>
-        </div>
-      </div>
     </div>
-    </template>
+</template>
     
-    <script lang="ts">
-    import Switch from '../lib/Switch.vue'
-    import Button from '../lib/Button.vue'
-    import Switch1Demo from './Switch1.demo.vue'
-    import Switch2Demo from './Switch2.demo.vue'
-    import {
-      ref
-    } from 'vue'
-    export default {
-      components: {
-    Switch,
-    Button,
-    Switch1Demo,
-    Switch2Demo
-},
-      setup() {
+<script lang="ts">
+import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
+import {
+    ref
+} from 'vue'
+export default {
+    components: {
+        Button,
+    },
+    setup() {
         const bool = ref(false)
         return {
-          bool,Switch1Demo,Switch2Demo
+            bool, Switch1Demo, Switch2Demo
         }
-      }
     }
-    </script>
+}
+</script>
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
+
 .demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  >h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    >pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+
+    >h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
     }
-  }
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+
+        >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
 }
 </style>
