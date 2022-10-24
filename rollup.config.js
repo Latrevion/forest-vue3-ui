988a95df9c6a8bbee3512c9ef34c9de3f05fd25a
Latrevion <xiaopikaqiu@outlook.com>
2022-10-24 21:45:51 +0800

    更新rollup.config.js,解决打包失败问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
-import esbuild from 'rollup-plugin-esbuild'
-import vue from 'rollup-plugin-vue'
-import scss from 'rollup-plugin-scss'
-import dartSass from 'sass';
-import { terser } from "rollup-plugin-terser"
+const esbuild = require("rollup-plugin-esbuild");
+const vue = require("rollup-plugin-vue");
+const scss = require("rollup-plugin-scss");
+const dartSass = require( "sass");
+const{ terser } = require(  "rollup-plugin-terser");
 
-export default {
-  input: 'src/lib/index.ts',
+module.exports = {
+  input: "src/lib/index.ts",
   output: {
     globals: {
-      vue: 'Vue'
+      vue: "Vue",
     },
-    name: 'Forest',
-    file: 'dist/lib/forest.js',
-    format: 'umd',
-    plugins: [terser()]
+    name: "Forest",
+    file: "dist/lib/forest.js",
+    format: "umd",
+    plugins: [terser()],
   },
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
+    vue({
+        include: /\.vue$/,
+      }),
     esbuild({
       include: /\.[jt]s$/,
-      minify: process.env.NODE_ENV === 'production',
-      target: 'es2015' 
-    }),
-    vue({
-      include: /\.vue$/,
+      minify: process.env.NODE_ENV === "production",
+      target: "es2015",
     })
   ],
-}
+};
