420edd8c2f7ff1787ad390f848babb47c877bf27
Latrevion <xiaopikaqiu@outlook.com>
2022-10-25 16:37:13 +0800

    更新gitignore

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git init  &&
 git add . &&
 git commit -m 'update' &&
 git branch -M master &&
-git remote add origin git@github.com:Latrevion/forest-ui-website.git &&
+git remote add origin git@gitee.com:latrevion/forest-ui-website.git &&
 git push -f -u origin master &&
 cd ..
-echo https://latrevion.github.io/forest-ui-website/index.html
